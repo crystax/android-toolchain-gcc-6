53c9c2e6cd8317fcbed061c2b81f857e8a528cf3
H.J. Lu <hongjiu.lu@intel.com>
2014-04-14 15:59:47 -0700

    [android] Always enable --eh-frame-hdr for static executable
    See 5e6cdf76af295c9a39b695ca228cff675e8ff4ae and
    23e3137ee2897464b051599b85a09f130d3ad05d
    
    Change-Id: Ibda473188e5a10f2a0592f2494ad00ad1f91e04b
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ gcc/testsuite/g++.dg/eh/spec3-static.C
@@ -0,0 +1,25 @@
+// PR c++/4381
+// Test that exception-specs work properly for classes with virtual bases.
+
+// { dg-do run }
+// { dg-options "-static" }
+
+class Base {};
+
+struct A : virtual public Base
+{
+  A() {}
+};
+
+struct B {};
+
+void func() throw (B,A)
+{
+  throw A();
+}
+
+int main(void)
+{
+  try {	func(); }
+  catch (A& a) { }
+}
