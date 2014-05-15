8e659bb583613b584267c4e16febf341fc2589f0
Carrot Wei <carrot@google.com>
2014-05-15 10:06:33 +0800

    [android] Add simplify-got
    This pass optimize GOT_PREL (already exists in toolchain/gcc/gcc-4.6)
    Backport from svn://gcc.gnu.org/svn/gcc/branches/google/gcc-4_6-mobile
    UNSPEC_GOT_PREL_SYM is now in new file arm/unspecs.md
    4.9 port is slightly different due to changes in gcc passes
    See Google b/14811006
    
    r173209 | carrot | 2011-04-30 16:07:46 +0800 (Sat, 30 Apr 2011) | 21 lines
    
    	* hooks.c (hook_rtx_void_null): New function.
    	* hooks.h (hook_rtx_void_null): New prototype.
    	* target.def (got_access): New hook vector declaration.
    	* tree-pass.h (pass_simplify_got): New pass.
    	* timevar.def (TV_SIMPLIFY_GOT): New TV id.
    	* simplify-got.c: New source file.
    	* Makefile.in (simplify-got.o): Add a new file.
    	* passes.c (init_optimization_passes): Add a new pass.
    	* config/arm/arm.c (arm_output_addr_const_extra): Output GOT_PREL
    	relocation.
    	(arm_get_pic_reg): New function.
    	(arm_clear_pic_reg): New function.
    	(arm_can_simplify_got_access): New function.
    	(arm_loaded_global_var): New function.
    	(arm_load_global_address): New function.
    	* config/arm/arm.md (UNSPEC_GOT_PREL_SYM): New UNSPEC symbol.
    
    	* testsuite/gcc.target/arm/got1.c: New testcase.
    	* testsuite/gcc.target/arm/got2.c: New testcase.
    
    Change-Id: I91e881df19bb6937a5fbcc8e6b83d158717c7773
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- gcc/hooks.h
+++ gcc/hooks.h
@@ -105,6 +105,7 @@ extern bool default_can_output_mi_thunk_no_vcall (const_tree, HOST_WIDE_INT,
 extern rtx hook_rtx_rtx_identity (rtx);
 extern rtx hook_rtx_rtx_null (rtx);
 extern rtx hook_rtx_tree_int_null (tree, int);
+extern rtx hook_rtx_void_null (void);
 
 extern char *hook_charptr_void_null (void);
 extern const char *hook_constcharptr_void_null (void);
